232f5f411d1875eda8cd5095ef637fdd4551b81d
Zhen Tang <zt438@nyu.edu>
2020-02-10 17:24:22 -0500

    Updated the readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Canny Edge Detector
 
-Implementation of Canny's Edge Detector.
+Implementation of Canny's Edge Detector using Python 3.
 
 The result of each step of the edge detector is saved.
 
--- clean.sh
+++ clean.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+# Simple script to clear out the outputs of the canny edge detector program.
+
 rm binary_edge_map.bmp gaussian_smooth.bmp horizontal_gradient.bmp vertical_gradient.bmp gradient_magnitude.bmp post_suppression_magnitude.bmp
